1f0ee8e38875c80ff04cd7f450d3f23f013f110e
Medhansh Sharma <sharmamedhansh21@gmail.com>
2024-07-25 18:49:45 +0530

    Weather API

---
+++ journalApp/src/main/java/com/edigest/journalApp/apiresponse/WeatherResponse.java
@@ -0,0 +1,78 @@
+package com.edigest.journalApp.apiresponse;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeatherResponse {
+
+    @JsonProperty("current")
+    private Current current;
+    @JsonProperty("condition")
+    private Condition condition;
+
+    @Getter
+    @Setter
+    public  static class Condition {
+        @JsonProperty("text")
+        private String text;
+
+        @JsonProperty("icon")
+        private String icon;
+
+        @JsonProperty("code")
+        private int code;
+
+    }
+    @Setter
+    @Getter
+    public  static class Current {
+
+        @JsonProperty("temp_c")
+        private double tempC;
+
+        @JsonProperty("temp_f")
+        private double tempF;
+
+        @JsonProperty("is_day")
+        private int isDay;
+
+        @JsonProperty("condition")
+        private Condition condition;
+
+        @JsonProperty("wind_mph")
+        private double windMph;
+
+        @JsonProperty("wind_kph")
+        private double windKph;
+
+        @JsonProperty("wind_degree")
+        private int windDegree;
+
+        @JsonProperty("wind_dir")
+        private String windDir;
+
+
+        @JsonProperty("humidity")
+        private int humidity;
+
+        @JsonProperty("cloud")
+        private int cloud;
+
+        @JsonProperty("feelslike_c")
+        private double feelsLikeC;
+
+        @JsonProperty("feelslike_f")
+        private double feelsLikeF;
+
+        @JsonProperty("windchill_c")
+        private double windChillC;
+
+        @JsonProperty("windchill_f")
+        private double windChillF;
+
+
+    }
+}
--- journalApp/src/main/java/com/edigest/journalApp/controller/UserContoller.java
+++ journalApp/src/main/java/com/edigest/journalApp/controller/UserContoller.java
@@ -1,14 +1,13 @@
 package com.edigest.journalApp.controller;
 
+import com.edigest.journalApp.apiresponse.WeatherResponse;
+import com.edigest.journalApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.edigest.journalApp.entity.User;
 import com.edigest.journalApp.repository.UserRepository;
@@ -24,6 +23,9 @@ public class UserContoller {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private WeatherService weatherService;
 
 
     @PutMapping
@@ -44,6 +46,18 @@ public class UserContoller {
         String userName = authentication.getName();
         userRepository.deleteByUserName(userName);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @GetMapping
+    public ResponseEntity<?> greeting() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        WeatherResponse weatherResponse=weatherService.getWeather("Mumbai");
+
+    if(weatherResponse !=null){
+            String greeting="hi "+authentication.getName()+" Todays Weather is "+weatherResponse.getCurrent().getCondition().getText();
+            return new ResponseEntity<>(greeting,HttpStatus.OK);
+        }else{
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
     }
 }
 
